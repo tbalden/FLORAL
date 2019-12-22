fb54e7370b752ed8761e42cb2de574a4b96da9a0
tbalden <illespal@gmail.com>
2019-12-22 06:01:57 -0800

    build, defconf: simplify build / conf no LTO/CFI/SCS

--- build_kirisakura.sh
+++ build_kirisakura.sh
@@ -4,7 +4,7 @@ echo
 echo "Clean Build Directory"
 echo 
 
-make clean && make mrproper
+#make clean && make mrproper
 
 echo
 echo "Issue Build Commands"
@@ -13,13 +13,15 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=/media/miles/Storage/CLANG_TC/tc-build/install/bin
-export PATH=${CLANG_PATH}:${PATH}
-export DTC_EXT=/home/miles/Downloads/wahoo-kernel-tools/bin/dtc
+BASE_PATH=/home/android/pixel
+EXT_UTILS=$BASE_PATH/wahoo-kernel-tools/bin
+export CLANG_PATH=$BASE_PATH/clang-10/bin
+export PATH=${CLANG_PATH}:${EXT_UTILS}:${PATH}
+export DTC_EXT=$EXT_UTILS/dtc
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=~/Android_Build/GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-export LD_LIBRARY_PATH=/media/miles/Storage/CLANG_TC/tc-build/install/lib:$LD_LIBRARY_PATH
+export CROSS_COMPILE=$BASE_PATH/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=$BASE_PATH/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export LD_LIBRARY_PATH=$BASE_PATH/clang-10/lib:$LD_LIBRARY_PATH
 
 echo
 echo "Set DEFCONFIG"
